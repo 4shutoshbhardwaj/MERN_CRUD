836bf1ee75ba978f85119ee686fe7f93e9725db0
4shutoshbhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2022-11-06 19:12:53 +0530

    Done

--- client/src/components/Ashutoshbhardwaj.jsx
+++ client/src/components/Ashutoshbhardwaj.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Ashutoshbhardwaj = () => {
+
+  const LinkStyle={
+    color:"white",
+    textDecoration:"none"
+  }
+  
   return (
-    <div>Ashutoshbhardwaj</div>
+    <div style={{height:"89.9vh",display:"flex",alignItems:'center',justifyContent:"space-around",backgroundColor:"black"}}>
+    <div>
+    <h2 style={LinkStyle}>Check out my GitHub profile for more projects</h2>
+    <h1 style={LinkStyle}>GitHub link :- <a href="https://github.com/4shutoshbhardwaj" style={LinkStyle}><h1>4shutoshbhardwaj</h1></a></h1>
+    </div>
+    </div>
   )
 }
 
